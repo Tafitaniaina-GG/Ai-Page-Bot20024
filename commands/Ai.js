51c227453cd7183691bf9c14f66295b362f5bd65
Nicsbot12 <154717275+Nicsbot12@users.noreply.github.com>
2024-10-01 18:54:14 +0800

    Create Ai.js

---
+++ commands/Ai.js
@@ -0,0 +1,37 @@
+const axios = require('axios');
+
+module.exports = {
+  name: 'Ai',
+  description: 'Ask a question to Ai',
+  author: 'Nics',
+  async execute(senderId, args, pageAccessToken, sendMessage) {
+    const prompt = args.join( );
+    try {
+      const apiUrl = `https://markdevs-last-api.onrender.com/api/v3/gpt4?ask=${encodeURIComponent(prompt)}`;
+      const response = await axios.get(apiUrl);
+      const text = response.data;
+
+      // Split the response into chunks if it exceeds 2000 characters
+      const maxMessageLength = 2000;
+      if (text.length > maxMessageLength) {
+        const messages = splitMessageIntoChunks(text, maxMessageLength);
+        for (const message of messages) {
+          sendMessage(senderId, { text: message }, pageAccessToken);
+        }
+      } else {
+        sendMessage(senderId, { text }, pageAccessToken);
+      }
+    } catch (error) {
+      console.error('Error calling GPT-4 API:', error);
+      sendMessage(senderId, { text: 'Please Enter Your Valid Question?.' }, pageAccessToken);
+    }
+  }
+};
+
+function splitMessageIntoChunks(message, chunkSize) {
+  const chunks = [];
+  for (let i = 0; i < message.length; i += chunkSize) {
+    chunks.push(message.slice(i, i + chunkSize));
+  }
+  return chunks;
+                    }
